c9d544d59f7352cf22def8cf8a012f79f1c2f225
AntonZotin <toxin88@mail.ru>
2024-03-13 12:09:24 +0300

    add algo task2

---
+++ src/main/java/algo/task2/MegaArray.java
@@ -0,0 +1,71 @@
+package algo.task2;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+public class MegaArray {
+    private final Logger logger = Logger.getLogger(MegaArray.class.getName());
+    private final int[] array;
+
+    public MegaArray(int[] values) {
+        array = values;
+    }
+
+    public void mergeSort() {
+        sort(array, array.length);
+    }
+
+    private int[] sort(int[] arr, int length) {
+        if (length < 2) {
+            return arr;
+        }
+        int midIndex = length / 2;
+        int[] left = new int[midIndex];
+        int[] right = new int[length - midIndex];
+
+        for (int i = 0; i < midIndex; i++) {
+            left[i] = arr[i];
+        }
+        for (int i = midIndex; i < length; i++) {
+            right[i - midIndex] = arr[i];
+        }
+        sort(left, midIndex);
+        sort(right, length - midIndex);
+
+        merge(arr, left, right, midIndex, length - midIndex);
+        return arr;
+    }
+
+    private void merge(int[] arr, int[] leftArr, int[] rightArr, int left, int right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+            }
+        }
+        while (i < left) {
+            arr[k++] = leftArr[i++];
+        }
+        while (j < right) {
+            arr[k++] = rightArr[j++];
+        }
+    }
+
+    public int binarySearch(int value) {
+        return search(array, value, 0, array.length);
+    }
+
+    private int search(int[] arr, int value, int low, int high) {
+        int middle = low + ((high - low) / 2);
+        if (high < low) return -1;
+        if (value == arr[middle]) return middle;
+        else if (value < arr[middle]) return search(arr, value, low, middle - 1);
+        else return search(arr, value, middle + 1, high);
+    }
+
+    public void print() {
+        logger.info(Arrays.toString(array));
+    }
+}
---
+++ src/test/java/algo/task2/MegaArrayTest.java
@@ -0,0 +1,24 @@
+package algo.task2;
+
+import java.util.Random;
+import org.junit.jupiter.api.Test;
+
+class MegaArrayTest {
+    final int LENGTH = 50;
+
+    @Test
+    void megaArrayTest() {
+        Random random = new Random();
+        int[] values = new int[LENGTH];
+        for (int i = 0; i < LENGTH; i++) {
+            values[i] = random.nextInt(LENGTH * 2);
+        }
+        MegaArray array = new MegaArray(values);
+        array.print();
+        array.mergeSort();
+        array.print();
+        for (int i = 1; i < 30; i++) {
+            System.out.println(i + " has index " + array.binarySearch(i));
+        }
+    }
+}
